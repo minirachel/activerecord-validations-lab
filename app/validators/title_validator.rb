1889718fe3af4f3107f9b7da18a8a9580dfaa4d6
minirachel <rachel.a.levinson@gmail.com>
2021-08-02 11:56:38 -0400

    commit tree 8e5d645b720148f5891e5a01d978a2c4b6112cf7

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,14 +1,27 @@
-class TitleValidator < ActiveModel::Validator 
-    def validate(record)
-       if CLICKBAIT.none? (|p| p.match record.title)
-        record.errors[:title] << "Please write stronger clickbait."
-        end
-    end
+require 'pry'
 
+class TitleValidator < ActiveModel::Validator 
+    
     CLICKBAIT = [
-        /Won't Believe/i,
         /Secret/i, 
+        /Won't Believe/i,
         /Top \d+ /i, 
         /Guess/i,
     ]
+
+    def validate(record)
+        if record.title != nil
+            # binding.pry
+            if is_clickbait?(record) == []
+                record.errors[:title] << "Please write stronger clickbait."
+            end
+        end
+    end
+
+    def is_clickbait?(record)
+        CLICKBAIT.select do |c|
+            record.title.match(c)
+        end
+    end
+
 end
